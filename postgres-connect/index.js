3b37cec36f3c01f762b2ea1e6d5f273325e38ecc
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2023-09-12 15:46:15 -0500

    refactor: add parseRow

--- postgres-connect/index.js
+++ postgres-connect/index.js
@@ -28,6 +28,26 @@ let chunk = (arr, chunkSize) => {
     return res;
 }
 
+let parseRow = (row) => {
+    return {
+        age: parseInt(row.Age),
+        gender: row.Gender,
+        annual_income: parseFloat(row.Income),
+        credit_score: parseInt(row.CreditScore),
+        credit_history_length: parseInt(row.CreditHistoryLength),
+        loans_count: parseInt(row.NumberOfExistingLoans),
+        loan_amount: parseFloat(row.LoanAmount),
+        loan_tenure: parseInt(row.LoanTenure),
+        is_customer: row.ExistingCustomer,
+        state: row.State,
+        city: row.City,
+        ltv: parseFloat(row.LTVRatio),
+        employment_profile: row.EmploymentProfile,
+        profile_score: parseInt(row.ProfileScore),
+        occupation: row.Occupation
+    }
+}
+
 
 
 let loadDataFromCsv = (csvFile) => {
@@ -39,23 +59,7 @@ let loadDataFromCsv = (csvFile) => {
 
     pipeline.on('data', (row) => {
         if (!row) return;
-        rows.push({
-            age: parseInt(row.Age),
-            gender: row.Gender,
-            annual_income: parseFloat(row.Income),
-            credit_score: parseInt(row.CreditScore),
-            credit_history_length: parseInt(row.CreditHistoryLength),
-            loans_count: parseInt(row.NumberOfExistingLoans),
-            loan_amount: parseFloat(row.LoanAmount),
-            loan_tenure: parseInt(row.LoanTenure),
-            is_customer: row.ExistingCustomer,
-            state: row.State,
-            city: row.City,
-            ltv: parseFloat(row.LTVRatio),
-            employment_profile: row.EmploymentProfile,
-            profile_score: parseInt(row.ProfileScore),
-            occupation: row.Occupation
-        });
+        rows.push(parseRow(row));
         process.stdout.write(`Reading ${rowCount++} rows \r`);
     });
 
