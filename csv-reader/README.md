addfdea7791e11770b010b420e919d416f36d9ff
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2023-09-12 15:37:43 -0500

    feat: add csv-reader

---
+++ csv-reader/README.md
@@ -0,0 +1,5 @@
+# Use of csv-parser
+
+
+# References
+* https://www.npmjs.com/package/csv-parser
---
+++ csv-reader/index.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import csv from 'csv-parser';
+
+// Función para leer y cargar datos desde un archivo CSV
+let cargarDatosDesdeCSV = (rutaArchivo) => {
+    let count = 0;
+    fs.createReadStream(rutaArchivo)
+        .pipe(csv({ headers: false }))
+        .on('data', async (row) => {
+            process.stdout.write(`Leyendo ${count++} filas \r\n`);
+            console.log(row);
+        })
+        .on('end', () => {
+            console.log('Finish process');
+        });
+}
+
+// Llama a la función para cargar datos desde el archivo CSV
+if (process.argv[2] && process.argv[2] === '-f') {
+    console.log('Loading Data from:', process.argv[3]);
+    cargarDatosDesdeCSV(process.argv[3]);
+} else {
+    console.error('Flag -f with file name is required example: node index -f data.csv')
+}
