ba321cbb5f25ed1e4fb83cdde8665bf2e9ae0a05
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2023-09-21 16:36:19 -0500

    refactor: update fields

--- postgres-connect/bd_init.sql
+++ postgres-connect/bd_init.sql
@@ -1,16 +1,16 @@
-CREATE TABLE credit_table  (
+CREATE TABLE credit_profile  (
     age integer,
     gender varchar(10),
-    annual_income numeric(10, 2),
+    income numeric(10, 2),
     credit_score integer,
     credit_history_length integer,
-    loans_count integer,
+    number_of_existing_loans integer,
     loan_amount bigint,
     loan_tenure integer,
-    is_customer varchar(20),
+    existing_customer varchar(20),
     state varchar(100),
     city varchar(100),
-    ltv numeric(15,5),
+    ltv_ratio numeric(15,5),
     employment_profile varchar(200),
     profile_score integer,
     occupation varchar(200)
